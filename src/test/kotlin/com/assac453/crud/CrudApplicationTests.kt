4f7e6f8a6693be62e0e03515f9f5f3d8f070b54b
assac453 <ritterfrosch@yandex.ru>
2024-02-20 20:46:15 +0300

    feat: add tests

--- src/test/kotlin/com/assac453/crud/CrudApplicationTests.kt
+++ src/test/kotlin/com/assac453/crud/CrudApplicationTests.kt
@@ -1,13 +1,52 @@
 package com.assac453.crud
 
+import com.assac453.crud.dto.TaskDTORequest
+import com.assac453.crud.dto.TaskDTOResponse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class CrudApplicationTests {
+@SpringBootTest(
+	classes = [CrudApplication::class],
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+class CrudApplicationTests(@Autowired var restTemplate: TestRestTemplate) {
 
 	@Test
-	fun contextLoads() {
+	fun createTaskShouldReturnTrue() {
+
+
+
+		var taskDTORequest = TaskDTORequest(name = "Task", description = "Description", done = false)
+		var result = this.restTemplate.postForEntity("/tasks/create", taskDTORequest, TaskDTOResponse::class.java)
+		val taskId = result.body?.id!!
+		assertTrue { result.body?.name.equals("Task") }
+		assertTrue { result.body?.description.equals("Description") }
+
+
+
+		result = this.restTemplate.getForEntity("/tasks/{id}", TaskDTOResponse::class.java, taskId)
+		assertTrue{ result.body?.name.equals("Task") }
+
+
+
+		taskDTORequest = TaskDTORequest(name = taskDTORequest.name, description = taskDTORequest.description, done = !taskDTORequest.done)
+		this.restTemplate.put("/tasks/{id}", taskDTORequest, taskId)
+		result = this.restTemplate.getForEntity("/tasks/{id}", TaskDTOResponse::class.java ,taskId)
+		assertTrue{ result.statusCode.is2xxSuccessful}
+		assertTrue{ result.body?.done!! }
+
+
+
+		this.restTemplate.delete("/tasks/{id}", taskId)
+		val result1 = this.restTemplate.getForEntity("/tasks/{id}", String::class.java, taskId)
+		assertTrue{ result1.statusCode == HttpStatus.NOT_FOUND }
+
 	}
+
 
 }
